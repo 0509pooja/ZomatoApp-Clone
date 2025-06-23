4e8be3dca5663e848ed6f89081576cf180c379e1
0509pooja <149455505+0509pooja@users.noreply.github.com>
2025-06-23 11:31:42 +0530

    Update iam-policy.tf

--- EKS-TF/iam-policy.tf
+++ EKS-TF/iam-policy.tf
@@ -17,3 +17,28 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.NodeGroupRole.name
 }
+resource "aws_iam_policy" "EKSAccessPolicy" {
+  name        = "EKSAccessPolicy"
+  description = "Allows GitHub EC2 Runner to access EKS cluster"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "eks:ListClusters",
+          "eks:DescribeCluster",
+          "eks:AccessKubernetesApi"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "sts:AssumeRole"
+        ],
+        Resource = aws_iam_role.EKSClusterRole.arn
+      }
+    ]
+  })
+}
